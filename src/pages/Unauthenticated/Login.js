83f850d3a80806683b3da9a2a5f03f58fb6fb69b
Storn White <stornwhite@Storns-Awesome-Computer-Box.local>
2021-04-21 13:12:21 -0700

    [EXP-10] Authentication routes

---
+++ src/pages/Unauthenticated/Login.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import { UnauthenticatedPage } from './UnauthenticatedPage';
+
+/** ============================ Components ================================ */
+export const LoginPage = () => {
+
+  return (
+    <UnauthenticatedPage>
+
+      <h1>This is the Login Page!</h1>
+        
+    </UnauthenticatedPage>
+  );
+
+};
